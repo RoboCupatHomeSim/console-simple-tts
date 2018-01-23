25230d0f8146553bdc34f39d1fd0e701a06c99ee
Hiroki Yamada <yamada4tec@gmail.com>
2018-01-23 15:41:50 +0900

    first commit

---
+++ ConsoleSimpleTTS/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using SpeechLib;
+
+namespace ConsoleSimpleTTS
+{
+	class Program
+	{
+		/// <summary>
+		/// Simple TTS 
+		/// </summary>
+		/// <param name="args">args[0]: Message, args[1]: Attributes e.g. "Language=409;Gender=Female"</param>
+		static void Main(string[] args)
+		{
+			SpVoice voice = new SpVoice();
+
+			voice.Volume = 100; // Volume
+			voice.Rate = 0 ;    // Rate
+
+			SpObjectTokenCategory tokenCat = new SpObjectTokenCategory();
+			tokenCat.SetId(SpeechLib.SpeechStringConstants.SpeechCategoryVoices, false);
+
+			string requiredAttributes = null;
+
+			if(args.Length > 1)
+			{
+				// Select tokens
+				//     (Language:409=English (United States)/411=Japanese, Gender:Male/Female ... )
+				requiredAttributes = args[1];
+			}
+
+			ISpeechObjectTokens tokens = tokenCat.EnumerateTokens(requiredAttributes, null);
+
+			// No tokens
+			if(tokens.Count==0)
+			{
+				// Reselect
+				tokens = tokenCat.EnumerateTokens(null, null); // Get all tokens
+			}
+
+			voice.Voice = tokens.Item(0);
+
+			string message = "Hello World!";
+
+			if(args.Length > 0)
+			{
+				message = args[0];
+			}
+			voice.Speak(message, SpeechVoiceSpeakFlags.SVSFDefault);
+
+			Console.WriteLine("Finished");
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,39 @@
+# Simple Text-to-Speech
+
+This is a console application that speaks the given text using SAPI.  
+And this project can also generate the Interop.SpeechLib.dll.
+
+
+## Prerequisites
+
+- Windows 10
+- Visual Studio 2017
+- Microsoft Speech Platform  
+    You can download installers for speech synthesis from the following link.  
+    https://www.microsoft.com/en-us/download/details.aspx?id=27224
+
+## How to Build
+
+1. Launch Visual Studio 2017.
+2. Open the ConsoleSimpleTTS.sln with Visual Studio.
+3. Click [Build]-[Build Solution].
+4. ConsoleSimpleTTS.exe and Interop.SpeechLib.dll are generated in a "ConsoleSimpleTTS\bin\Release" folder.
+
+## How to Execute
+
+1. Launch a command prompt.
+2. You can confirm Text-to-Speech by executing the following command.  
+```
+> cd ConsoleSimpleTTS\bin\Release
+> ConsoleSimpleTTS "I have a pen"
+```
+3. You can also set options to the second argument.  
+The 409 means English (United States).  
+```
+> ConsoleSimpleTTS "I have a pen" "Language=409;Gender=Female"
+```
+
+
+## License
+
+This project is licensed under the SIGVerse License - see the LICENSE.txt file for details.
